df20dfd5bba70e3afef2f4cc9b495f6d85878350
rakib-utsho <rakibutsho1920@gmail.com>
2024-01-14 05:24:01 +0600

    update code

--- code/code.ino
+++ code/code.ino
@@ -19,3 +19,22 @@ float duration;
 float distance;
 float Length;
 float BMI;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("ready to read and please ensure the weight more than 1 Kg");
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+
+//  Serial.println("Remove all weight from scale");
+//  Serial.println("After readings begin, place known weight on scale");
+//  Serial.println("Press + or a to increase calibration factor");
+//  Serial.println("Press - or z to decrease calibration factor");
+
+  scale.set_scale(calibration_factor);
+  scale.tare();  //Reset the scale to 0
+
+ // long zero_factor = scale.read_average(); //Get a baseline reading
+  //Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  //Serial.println(zero_factor);
+}
